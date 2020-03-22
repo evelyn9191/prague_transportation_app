296181b61aff48a9a06d8eec2d7c0a0b9a7aae9d
Michaela Ockova <ockova.michaela@gmail.com>
2020-03-22 09:57:01 +0100

    Refactor: put data importing logic to one module

--- transport_app/import_gtfs_data.py
+++ transport_app/import_gtfs_data.py
@@ -1,15 +1,60 @@
-from gtfspy import import_gtfs
+import json
+import logging
+from datetime import datetime
+
+import requests
+from dateutil import parser
+from gtfspy import import_gtfs, gtfs
+
+from transport_app import config
+
+logger = logging.getLogger(__file__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='{"time": "%(asctime)s.%(msecs)03d", '
+    '"name": "%(name)s", "level": "%(levelname)s", '
+    '"message": "%(message)s"}',
+    datefmt=f"{config.LOG_TIME_FORMAT} %H:%M:%S",
+)
 
 
-def load_or_import_example_gtfs(verbose=False):
-    imported_database_path = "transport.db"
+def get_latest_transport_data() -> bool:
+    with open(config.ZIP_DOWNLOAD_LOG) as zip_downloads:
+        logs = zip_downloads.readlines()
+        if not logs:
+            download_traffic_source()
+            logger.info("ZIP download success")
+            return True
+        last_update = json.loads(logs[-1])
+        download_date = parser.parse(last_update["time"])
+        today = datetime.today()
+        zip_update_time = datetime.strptime("18:00", "%H:%M").time()
+        if (
+            download_date.date() <= today.date()
+            and download_date.time() < zip_update_time < today.time()
+        ):
+            download_traffic_source()
+            logger.info("ZIP download success")
+            return True
+        return False
+
+
+def download_traffic_source() -> None:
+    traffic_file = requests.get(config.TRANSPORT_DATA_URL)
+    with open(config.TRANSPORT_DATA_ZIP, "wb") as f:
+        f.write(traffic_file.content)
+
+
+def import_gtfs_data(verbose=True):
     import_gtfs.import_gtfs(
-        ["../data/PID_GTFS.zip"],
-        imported_database_path,
-        print_progress=verbose,
-        location_name="Prague",
+        [config.TRANSPORT_DATA_ZIP], config.DB_NAME, print_progress=verbose, location_name="Prague",
     )
+    g = gtfs.GTFS(config.DB_NAME)
+    g.meta["download_date"] = datetime.today().date()
 
 
 if __name__ == "__main__":
-    load_or_import_example_gtfs(verbose=True)
+    data_updated = get_latest_transport_data()
+    if data_updated:
+        import_gtfs_data()
+    logger.debug("Transport data updated.")
